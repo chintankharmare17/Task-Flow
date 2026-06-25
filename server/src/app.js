1403da4f29eedcc8c291ae45732a4643297161c0
chintankharmare17 <chintankharmare009@gmail.com>
2026-06-26 00:18:26 +0530

    feat: setup backend foundation and local mongodb

---
+++ server/src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "🚀 TaskFlow API is running...",
+  });
+});
+
+export default app;
